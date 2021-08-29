9bdc99ea39b20c334aed9593ad7b06fb63f662c5
Gerardo Morales Gómez <moralesgerardomgz@outlook.com>
2021-08-29 03:01:24 -0500

    Mensajes de error

--- src/main/kotlin/mx/clientes/registro/controller_cliente/ClienteController.kt
+++ src/main/kotlin/mx/clientes/registro/controller_cliente/ClienteController.kt
@@ -6,10 +6,11 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Qualifier
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
+import org.springframework.validation.BindingResult
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestMapping
+import javax.validation.Valid
 
 @Controller
 class ClienteController {
@@ -44,9 +45,15 @@ class ClienteController {
 
 
     @PostMapping("/saveCliente")
-    fun procesoGuardar(cliente: Cliente): String {
-        this.clienteDao.nuevoCliente(cliente)
-        return "redirect:clientes"
+    fun procesoGuardar(@Valid cliente: Cliente, result: BindingResult): String {
+
+        return if (result.hasErrors()) {
+            "nuevoCliente/form"
+        } else {
+            this.clienteDao.nuevoCliente(cliente)
+            "redirect:clientes"
+        }
+
     }
 
 
--- src/main/kotlin/mx/clientes/registro/modelos_clientes/Cliente.kt
+++ src/main/kotlin/mx/clientes/registro/modelos_clientes/Cliente.kt
@@ -1,29 +1,34 @@
 package mx.clientes.registro.modelos_clientes
 
-import lombok.AllArgsConstructor
-import lombok.Getter
-import lombok.Setter
 import org.springframework.format.annotation.DateTimeFormat
 import java.io.Serializable
 import java.util.*
 import javax.persistence.*
+import javax.validation.constraints.NotEmpty
+import javax.validation.constraints.NotNull
 
 @Entity
 @Table(name = "clientes")
-@AllArgsConstructor
-@Setter
-@Getter
 class Cliente : Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_cliente", nullable = false)
     open var idCliente: Long? = null
+
+    @NotEmpty
+    @NotNull
     var nombre: String? = null
+
+    @NotEmpty
+    @NotNull
     var apellidos: String? = null
 
+    @NotEmpty
+    @NotNull
     @Column(unique = true, nullable = false)
     var correo: String? = null
 
+    @NotNull
     @Temporal(TemporalType.DATE)
     @Column(name = "fecha_registro_cliente", nullable = false, updatable = false)
     @DateTimeFormat(pattern = "yyyy-MM-dd")
